938e8cf8403f74b8d50ee74b50e1b8b065a009b5
0xYoyo <113042560+0xYoyo@users.noreply.github.com>
2023-08-08 13:54:42 +0300

    add http req interceptor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import "./styles/App.css";
 import Nav from "./components/parts/Nav";
 import { Outlet } from "react-router-dom";
+import { useInterceptor } from "./utils/useInterceptor";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  useInterceptor();
 
   useEffect(() => {
     setLoading(false);
